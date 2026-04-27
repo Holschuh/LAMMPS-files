87de6a982d4fdcf36b3e0d4fa1b8d98163bea578
Lennard <lennard.holschuh@imtek.uni-freiburg.de>
2026-04-27 13:00:35 +0200

    Add files via upload

--- fix_contact_roll_93.cpp
+++ fix_contact_roll_93.cpp
@@ -100,6 +100,9 @@ FixContactRoll93::FixContactRoll93(LAMMPS *lmp, int narg, char **arg) :
   scalar_flag = 1;
   vector_flag = 1;
   size_vector = 4;
+  global_freq = 1;
+  extscalar = 1;
+  extvector = 1;
 
   force_flag = 0;
   for (int i = 0; i < 5; i++) {
